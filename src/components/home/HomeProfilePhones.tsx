6fa8d1f10959574a5dbef8b758c556b44af6d2c8
kasper171 <rodrigodiscord01@gmail.com>
2026-07-07 17:48:04 -0300

    deploy: redeploy completo 2026-07-07 17:48

--- src/components/home/HomeProfilePhones.tsx
+++ src/components/home/HomeProfilePhones.tsx
@@ -217,7 +217,6 @@ function PhonePreview({
           <div className="home-phone-shot-wrap">
             <img src={src} alt={alt} className="home-phone-shot" draggable={false} />
           </div>
-          <div aria-hidden className="home-phone-bottom-fade" />
         </div>
       </div>
     </div>
